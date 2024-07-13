ee51ae5699abf56434f21974d7ef2fb5a7a7f7a4
Your Name <you@example.com>
2024-07-13 00:48:52 -0400

    link about page to home page, implement static loading

--- passgen/generator/views.py
+++ passgen/generator/views.py
@@ -8,4 +8,12 @@ def about(request):
     return render(request, 'about.html')
 
 def password(request):
-    return render(request, 'password.html')
+    length = int(request.GET.get('length'))
+    uppercase = request.GET.get('uppercase')
+    lowercase = request.GET.get('lowercase')
+    symbols = request.GET.get('symbols')
+    if length < 8:
+        return render(request, 'password.html', {'error': 'Password length must be at least 8 characters'})
+    else:
+        password = generate_password(length, uppercase, lowercase, symbols)
+        return render(request, 'password.html', {'password': password})
